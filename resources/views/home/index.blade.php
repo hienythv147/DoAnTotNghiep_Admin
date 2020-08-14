bcbbf982b894c4e67e004030f4ae6d7613668067
tranletanphat-CDTH17PMC <56817092+tranletanphat-CDTH17PMC@users.noreply.github.com>
2020-08-14 22:29:43 +0700

    #Update giao diện 2

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -132,9 +132,9 @@
             <div class="col-lg-12">
                 <div class="special-menu text-center">
                     <div class="button-group filter-button-group">
-                        <button class="active" data-filter="*">Tất cả</button>
-                        <button data-filter=".top-featured">Mới nhất</button>
                         <button data-filter=".best-seller">Bán chạy</button>
+                        <button data-filter=".top-featured">Mới nhất</button>
+                        <button hidden class="active" data-filter="*">Tất cả</button>
                     </div>
                 </div>
             </div>
